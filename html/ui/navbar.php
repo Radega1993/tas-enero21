ee681baf6dae86858d30a4ff3cc4e981102df66b
Radega <rauldearriba@gmail.com>
2021-01-30 00:01:06 +0100

    fix path

--- html/ui/navbar.php
+++ html/ui/navbar.php
@@ -2,7 +2,7 @@
 include_once 'header.php';
 ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-  <a class="navbar-brand" href="../index.php">
+  <a class="navbar-brand" href="../">
     <img class="logo" src="../assets/img/logo.png" alt="">
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,7 +12,7 @@ include_once 'header.php';
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mx-auto nav-text">
       <li class="nav-item active">
-        <a class="nav-link mx-3" href="../index.php">Read Secrets <span class="sr-only">(current)</span></a>
+        <a class="nav-link mx-3" href="../">Read Secrets <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link mx-3" href="../main/tellsecret.php">Tell a secret</a>
