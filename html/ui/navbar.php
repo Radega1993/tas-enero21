df7014997e06b3ee4f6d9420d1cde89d986f1a4d
raul <rauldearriba@gmail.com>
2021-01-30 21:49:22 +0100

    center elements

--- html/ui/navbar.php
+++ html/ui/navbar.php
@@ -10,7 +10,7 @@ include_once 'header.php';
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mx-auto nav-text">
+    <ul class="navbar-nav navbar-center nav-text">
       <li class="nav-item active">
         <a class="nav-link mx-3" href="../">Read Secrets <span class="sr-only">(current)</span></a>
       </li>
