f23788bbe769c8675ce071da0fbf6afe2c32a4c5
raul <rauldearriba@gmail.com>
2021-02-04 15:49:41 +0100

    vote

--- html/index.php
+++ html/index.php
@@ -93,7 +93,7 @@ include_once BASE_PATH . '/ui/header.php';
                 <div class="row">
                   <p class="span6 quote-text mb-0 pr-5 pl-5 text-responsive">
                     <?php echo $row[1] ?>
-                  </p>
+                  </p>action="<?php echo $_SERVER['PHP_SELF']; ?>"
                 </div>
                 <hr>
                 <div class="blog-post-actions">
@@ -104,12 +104,12 @@ include_once BASE_PATH . '/ui/header.php';
                   <p class="blog-post-bottom pull-right">
                     <?php if (!isset($_SESSION[$row[6]])){
                       ?>
-                      <!--
-                      <form method="post" class="pull-right">
+
+                      <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" class="pull-right">
                         <button type="submit" value="voteYes" class="badge quote-badge mr-3"><?php echo ucfirst($row[4]) ?> ❤</button>
                         <button type="submit" value="voteNo" class="badge quote-badge mr-3"><?php echo ucfirst($row[5]) ?> X</button>
                       </form> 
-                    -->
+
                       <span class="badge quote-badge mr-3"><?php echo ucfirst($row[4]) ?> ❤</span>
                       <span class="badge quote-badge mr-3"><?php echo ucfirst($row[5]) ?> X</span> 
                       <?php
